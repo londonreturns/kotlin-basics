5f1985066c376133734053c78709dd02b8e71676
londonreturns <subedioscar.47@gmail.com>
2023-03-26 23:19:27 +0545

    Variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main(){
-    println("Hello World")
+    val wholeNumber = 3
+    val bigNumber = 3L
+    val preciseDecimal = 3.33
+    val decimal = 3.33f
+    val iLoveVegetables = false
+    val iLovePizza = true
+    val iAmALetter = 'a'
+    println("$wholeNumber $bigNumber $preciseDecimal $decimal $iLoveVegetables $iLovePizza $iAmALetter")
 }
